e96f89289954edfb7e2e8edb137c56fc6202dc87
Mike Shaw <mike.walker.shaw@gmail.com>
2024-07-10 19:30:41 +0100

    implement graceful shutdown from echo docs

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
@@ -21,9 +22,6 @@ type PageData struct {
 func main() {
 	e := echo.New()
 	e.Logger.SetLevel(log.INFO)
-
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
 
 	e.Use(echoMiddleware.RequestIDWithConfig(echoMiddleware.RequestIDConfig{
 		Generator: func() string {
@@ -60,11 +58,18 @@ func main() {
 		return components.Render(c, http.StatusOK, components.Settings())
 	})
 
-	if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
-		e.Logger.Fatal("shutting down the server")
-	}
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
 
 	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	if err := e.Shutdown(ctx); err != nil {
 		e.Logger.Fatal(err)
 	}
